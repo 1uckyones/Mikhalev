b5339ca6a37386877d0a1409432405419c40b08f
Luckyones <mr.heromak@gmail.com>
2022-09-17 17:24:15 +0400

    Initial commit

---
+++ src/main/java/com/example/Mikhalev/MikhalevApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mikhalev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikhalevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikhalevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mikhalev/MikhalevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mikhalev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhalevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
